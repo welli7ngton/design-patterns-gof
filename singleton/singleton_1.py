0c37b828ed287b6c79ad0323bbe01d7be280360f
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-12 12:49:29 -0300

    aula um sobre singleton

---
+++ singleton/singleton_1.py
@@ -0,0 +1,25 @@
+"""
+O Singleton tem a intenção de garantir que uma classe tenha somente uma
+instância e fornece um ponto global de acesso para a mesma.
+"""
+
+
+class AppSettings:
+    _instace = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instace:
+            cls._instace = super().__new__(cls, *args, **kwargs)
+        return cls._instace
+
+    def __init__(self) -> None:
+        self.theme = 'dark'
+
+
+if __name__ == '__main__':
+    as1 = AppSettings()
+    as1.theme = 'light'
+    print(as1.theme)
+
+    as2 = AppSettings()
+    print(as1.theme)
